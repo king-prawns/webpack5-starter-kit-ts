f3c585ea3a181edb9489e1afebe4551d5fe2b521
king-prawns <giorgio.gamberoni@gmail.com>
2021-01-31 19:23:24 +0000

    upgrade to webpack5

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import './index.pcss';
 import myApp from './components/my-app/index';
 
-if (PRODUCTION) {
-  require('offline-plugin/runtime').install();
-}
-
 customElements.define('my-app', myApp);
