1a571ee2a7ba04ea469dae0f60c1245551942d67
king-prawns <giorgio.gamberoni@gmail.com>
2026-01-14 11:25:59 +0000

    upgrade packages

---
+++ postcss.config.cjs
@@ -0,0 +1,34 @@
+// Use dynamic import for ESM-only modules
+module.exports = async () => {
+  const postcssPresetEnv = (await import('postcss-preset-env')).default;
+  const cssnano = (await import('cssnano')).default;
+  const postcssEasyImport = require('postcss-easy-import');
+  const postcssResponsiveType = require('postcss-responsive-type');
+  const postcssReporter = require('postcss-reporter');
+  const stylelint = require('stylelint');
+
+  return {
+    plugins: [
+      stylelint(),
+      postcssEasyImport({
+        extensions: '.pcss',
+        plugins: [stylelint()]
+      }),
+      postcssResponsiveType(),
+      postcssPresetEnv({
+        browsers: '> 0.25%, not dead, not ie 11',
+        stage: 0,
+        autoprefixer: {
+          grid: true
+        }
+      }),
+      cssnano({
+        preset: ['advanced', { zindex: false }]
+      }),
+      postcssReporter({
+        clearReportedMessages: true
+      })
+    ]
+  };
+};
+
